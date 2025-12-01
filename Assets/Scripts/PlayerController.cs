1c3f4188ccc19fd8e033714962d863fd07f2a333
gabrieljimenezfg <gabrieljimenez.fg@gmail.com>
2025-12-01 11:36:40 +0100

    terrain

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float cameraSensitivity;
     [SerializeField] private float speed;
     [SerializeField] private Transform followTarget;
+    [SerializeField] private float lerpSpeed;
 
     private void Awake()
     {
@@ -27,10 +28,17 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         Vector2 leftStickInput = playerInput.actions[MOVEMENT_ACTION_NAME].ReadValue<Vector2>();
-        
-        animator.SetFloat(ANIMATOR_HORIZONTAL, leftStickInput.x);
-        animator.SetFloat(ANIMATOR_VERTICAL, leftStickInput.y);
-        
+
+        var currentHorizontal = animator.GetFloat(ANIMATOR_HORIZONTAL);
+        var currentVertical = animator.GetFloat(ANIMATOR_VERTICAL);
+
+        var lerpSpeedMultiplier = lerpSpeed * Time.deltaTime;
+        var newHorizontal = Mathf.Lerp(currentHorizontal, leftStickInput.x, lerpSpeedMultiplier);
+        var newVertical = Mathf.Lerp(currentVertical, leftStickInput.y, lerpSpeedMultiplier);
+
+        animator.SetFloat(ANIMATOR_HORIZONTAL, newHorizontal);
+        animator.SetFloat(ANIMATOR_VERTICAL, newVertical);
+
         Vector3 movement = ((transform.forward * leftStickInput.y) + (transform.right * leftStickInput.x)) * speed;
         rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z);
     }
@@ -38,7 +46,7 @@ public class PlayerController : MonoBehaviour
     private void LateUpdate()
     {
         Vector2 lookInput = playerInput.actions[CAMERA_LOOK].ReadValue<Vector2>();
-        
+
         followTarget.localEulerAngles += new Vector3(lookInput.y * cameraSensitivity * Time.deltaTime, 0, 0);
         transform.eulerAngles += new Vector3(0, lookInput.x * cameraSensitivity * Time.deltaTime, 0);
     }
