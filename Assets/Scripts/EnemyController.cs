57436baf5af5a957b4737cd50b47d1d182a3c03d
gabrieljimenezfg <gabrieljimenez.fg@gmail.com>
2025-12-03 11:51:40 +0100

    raycast on trigger stay

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,21 +1,45 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    const string PLAYER_TAG = "Player";
+    private bool playerDetected;
     private Animator animator;
-    [SerializeField] private float speed;
     private Transform player;
+    private NavMeshAgent agent;
 
-    private void Awake()
+    private void Start()
     {
         animator = GetComponent<Animator>();
-        player = GameObject.FindWithTag("Player").transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
-        transform.LookAt(player.position);
-        transform.Translate(speed * Time.deltaTime * Vector3.forward);
+        if (playerDetected)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag(PLAYER_TAG)) return;
+
+        var playerDirection = (player.position - transform.position).normalized;
+        Ray ray = new Ray(transform.position + new Vector3(0, 1.65f, 0), playerDirection);
+
+        if (Physics.Raycast(ray, out var hit))
+        {
+            Debug.Log(hit.collider.name);
+            Debug.DrawRay(transform.position + new Vector3(0, 1.65f, 0), playerDirection * 100f, Color.red, 5f);
+            if (hit.transform.CompareTag(PLAYER_TAG))
+            {
+                playerDetected = true;
+            }
+        }
     }
 }
